eae5f22253c509c3b8f88dc24adb773fe87b4a30
Papish Limbu <papish.limbu@omnicommediagroup.com>
2024-09-30 13:30:27 +0545

    linked list

--- dsa/linked-list.js
+++ dsa/linked-list.js
@@ -53,8 +53,45 @@ class SingleLinkedList {
 const sll = new SingleLinkedList();
 sll.push(1);
 sll.push(2);
-sll.push(3);
-sll.push(4);
 sll.push(5);
-sll.pop(3);
-console.log(JSON.stringify(sll, 0, 2));
+
+const sll1 = new SingleLinkedList();
+sll1.push(1);
+sll1.push(3);
+sll1.push(5);
+
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    let tempNext = current.next;
+    current.next = prev;
+    prev = current;
+    current = tempNext;
+  }
+
+  return prev;
+}
+
+// console.log(JSON.stringify(reverseLinkedList(sll.head), 0, 2))
+
+function mergedSortedList(list1, list2) {
+  const node = new Node(0);
+  current = node;
+
+  while(list1 !== null && list2 !== null) {
+    if (list1.data < list2.data) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next
+    }
+    current = current.next;
+  }
+
+  console.log(JSON.stringify(node.next, 0, 2));
+}
+
+console.log(JSON.stringify(mergedSortedList(sll.head, sll1.head), 0, 2));
